06a162df9e91c415bb39fd60fc57cf2070de5ae5
Nithin1139 <87998375+Nithin1139@users.noreply.github.com>
2023-11-05 04:21:10 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,10 @@
 import styles from "./styles.module.css";
 import * as React from "react";
-import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
-import { deepOrange, deepPurple, grey } from "@mui/material/colors";
+import { deepOrange } from "@mui/material/colors";
 import Badge from "@mui/material/Badge";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
